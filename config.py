6adf9ee0836db39e61ab17277b9b2741a4d9a627
uhsiir <uhsiiir@gmail.com>
2024-12-14 19:51:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -39,7 +39,7 @@ DB_URI = environ.get("DB_URI", "mongodb+srv://xhere6969:w5cik73t88wIPo7y@cluster
 DB_NAME = environ.get("DB_NAME", "project 1")
 
 # Auto Delete Information
-AUTO_DELETE_MODE = bool(environ.get('AUTO_DELETE_MODE', . False)) # Set True or False
+AUTO_DELETE_MODE = bool(environ.get('AUTO_DELETE_MODE', False)) # Set True or False
 
 # If Auto Delete Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
 AUTO_DELETE = int(environ.get("AUTO_DELETE", "30")) # Time in Minutes
